97c837f1a398b8300cf2a254a5512e3554f4adb6
derrick <derrick.oswald@gmail.com>
2013-04-07 12:24:21 +0200

    Set up the input form.

--- thingmaker/ThingMaker.js
+++ thingmaker/ThingMaker.js
@@ -16,3 +16,46 @@ function ReadTorrent (torrent_file)
 
     return (string_torrent);
 }
+
+function ReadTorrentAsync (file, callback)
+{
+    var reader = new FileReader ();
+
+    // If we use onloadend, we need to check the readyState.
+    reader.onloadend = function (evt)
+    {
+        if (evt.target.readyState == FileReader.DONE) // DONE == 2
+        {
+            var torrent_file = evt.target.result;
+            var torrent = ReadTorrent (torrent_file);
+
+            callback (torrent);
+
+            var output = encode (torrent);
+            document.getElementById ('bencode_content').innerHTML = "<a href='data:application/octet-stream;base64,"
+                    + btoa (output) + "' download='output.torrent'>Torrent File</a>";
+
+            // generate the 160 bit (40 hex characters) info primary key
+            var info = torrent["info"];
+            var primary_key = sha1 (encode (info));
+
+            // spew it out as a magnet link URI
+            var thing_file = file.name; // default name is the file name
+            var title = torrent["info"]["thing"]["title"];
+            if (null == title)
+            {
+                var n = thing_file.lastIndexOf (".");
+                if (-1 == n)
+                    title = thing_file;
+                else
+                    title = thing_file.substring (0, n);
+            }
+            title = title.replace (/\&/g, "and");
+            title = title.replace (/ /g, "%20");
+            document.getElementById ('magnet').innerHTML = "<a href=\"" + "magnet:?xt=urn:btih:" + primary_key
+                    + "&dn=" + title + "\"  title=\"Download this torrent using magnet\">"
+                    + "<img id=\"magnet_icon\" src=\"images/magnet.png\">Magnet" + "</a>";
+        }
+    };
+    reader.readAsArrayBuffer (file);
+}
